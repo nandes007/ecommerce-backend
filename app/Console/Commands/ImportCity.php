f09ad81fa8bd6b9ecdd1b7cf46a1d5aff6493408
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2023-02-21 21:07:18 +0700

    import provice and city

--- app/Console/Commands/ImportCity.php
+++ app/Console/Commands/ImportCity.php
@@ -2,24 +2,24 @@
 
 namespace App\Console\Commands;
 
-use App\Models\Province;
+use App\Models\City;
 use Illuminate\Console\Command;
 
-class ImportProvince extends Command
+class ImportCity extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'import:province';
+    protected $signature = 'import:city';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Import Province';
+    protected $description = 'Import City';
 
     /**
      * Execute the console command.
@@ -28,7 +28,7 @@ class ImportProvince extends Command
      */
     public function handle()
     {
-        $file_path = resource_path("csvs/provinces.csv");
+        $file_path = resource_path("csvs/cities.csv");
         $file_contents = file_get_contents($file_path);
         $file_contents = str_replace("\r", "", $file_contents);
         $rows = explode("\n", $file_contents);
@@ -41,18 +41,20 @@ class ImportProvince extends Command
             }
 
             try {
-                $province = $column[1];
+                $province_id = $column[0];
+                $city = $column[1];
             } catch (\Exception $e) {
                 $this->error("CANNOT GET FILE FROM CSV");
                 continue;
             }
 
             $dataToInsert = [
-                'name' => $province
+                'province_id' => $province_id,
+                'name' => $city
             ];
 
-            Province::create($dataToInsert);
-            $this->info("PROVINCE HAS BEEN IMPORT SUCCESSFULLY");
+            City::create($dataToInsert);
+            $this->info("CITY HAS BEEN IMPORT SUCCESSFULLY");
         }
     }
 }
